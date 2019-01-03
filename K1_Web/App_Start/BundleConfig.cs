a32ba1ee4e46614a0680d57c32a5f7c582ee19dc
Triskryggg <kristrygg@gmail.com>
2019-01-03 12:54:33 +0000

    Test

--- K1_Web/App_Start/BundleConfig.cs
+++ K1_Web/App_Start/BundleConfig.cs
@@ -22,6 +22,10 @@ namespace K1_Web
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/main").Include(
+                      "~/Scripts/main.js"));
+
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
